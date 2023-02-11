a3c15fa614e18c97349d01bdcd198f368493228d
Derek Cross <derekcrosstest@gmail.com>
2023-02-11 10:34:34 -0500

    base files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl bg-green-500"> hello world ok!!!</h1>
+    </div>
+  )
+}
+
+export default App
