c89237d77665a2c249463fe694f3b27182266319
Derek Cross <derekcrosstest@gmail.com>
2023-02-11 11:02:47 -0500

    adding three module

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+
+function ThreeScene() {
+  return (
+    <Canvas>
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+    </Canvas>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl bg-green-500"> hello world!!!</h1>
+      <ThreeScene />
     </div>
   )
 }
