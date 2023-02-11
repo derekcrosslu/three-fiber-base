e373e746087b3931c497414e08e722450110e737
Derek Cross <derekcrosstest@gmail.com>
2023-02-11 10:39:09 -0500

    base test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl bg-green-500"> hello world ok!!!</h1>
+      <h1 className="text-3xl bg-green-500"> hello world!!!</h1>
     </div>
   )
 }
